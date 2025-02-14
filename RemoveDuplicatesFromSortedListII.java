ad77523e27e0106a43af1386c64d3aaaa903bd97
ArpitDhasmana <129339943+ArpitDhasmana@users.noreply.github.com>
2025-02-14 16:31:33 +0530

    Create RemoveDuplicatesFromSortedListII.java

---
+++ RemoveDuplicatesFromSortedListII.java
@@ -0,0 +1,33 @@
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head==null || head.next ==null){
+            return head;
+        }
+        ListNode dummy = new ListNode(0,head);
+        ListNode prev = dummy;
+        ListNode curr = head;
+        ListNode temp = head.next;
+        boolean flag = false;
+
+        while(temp != null){
+            if(curr.val != temp.val){
+                if(flag == true){
+                    prev.next = temp;
+                    flag = false;
+                }
+                else{
+                    prev = prev.next;
+                }
+            }
+            else if(curr.val == temp.val){
+                flag = true;
+            }
+            temp = temp.next;
+            curr = curr.next;
+        }
+        if(flag == true){
+            prev.next = temp;
+        }
+        return dummy.next;
+    }
+}
