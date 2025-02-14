1030997a4896cf8aebd916710b13bb9875fdf848
ArpitDhasmana <129339943+ArpitDhasmana@users.noreply.github.com>
2025-02-14 16:36:07 +0530

    Create SortList.java

---
+++ SortList.java
@@ -0,0 +1,27 @@
+class Solution {
+    public ListNode sortList(ListNode head) {
+        int len = 0;
+        ListNode temp = head;
+        while(temp != null){
+            temp = temp.next;
+            len++;
+        }
+        int[] listarray = new int[len];
+        int i = 0;
+        ListNode newtemp = head;
+        while(newtemp != null){
+            listarray[i] = newtemp.val;
+            i++;
+            newtemp = newtemp.next;
+        }
+        Arrays.sort(listarray);
+        ListNode temp2 = head;
+        int j = 0;
+        while(temp2 != null){
+            temp2.val = listarray[j];
+            j++;
+            temp2 = temp2.next;
+        }
+        return head;
+    }
+}
